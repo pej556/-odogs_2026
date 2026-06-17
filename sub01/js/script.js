65034c028eec83b86fd840e42ec00b3e60d6c3b5
pej556 <dmswn6173@naver.com>
2026-06-17 14:16:09 +0900

    Add files via upload

---
+++ sub01/js/script.js
@@ -0,0 +1,47 @@
+$(function(){
+
+  //돋보기
+  $(".pp").click(function(){
+    $(".search_box").show();
+    return false;
+  });
+  
+
+  $(".close").click(function(){
+     $(".search_box").hide();
+  });
+
+
+  //위로가기
+  $(".top").on("click",function(){
+  $('html, body').animate({
+  scrollTop : 0
+  },900)
+  return false;
+  });
+
+
+
+  //태블릿 헤더메뉴 열기
+  $(".tab_btn").click(function () {
+  if ($(".tab_menu_bar").is(":visible")) {
+    $(".tab_menu_bar").hide();
+  } else {
+    $(".tab_menu_bar").show();
+  }
+  return false;
+});
+  
+
+//태블릿 헤더메뉴 스크롤
+$(".tab_menu_bar span").click(function(){
+    $(this).parent().next().slideToggle();
+});
+
+  
+
+
+});
+
+
+
